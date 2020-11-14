2d614dca2dafa5032b303fa70b40c39a9386987b
ThomasFrau <32460955+ThomasFrau@users.noreply.github.com>
2020-11-14 17:04:04 +0000

    Updated tileset
    Added bendy rivers

--- generatorScreen.js
+++ generatorScreen.js
@@ -3,13 +3,24 @@
 const canvas = document.getElementById("generatorScreen");
 const context = canvas.getContext("2d");
 
-let temp = new Image()
+let tiles = new Image();
 
-temp.src = "Assets/tileSet/tileset.png";
+tiles.src = "Assets/tileSet/tileset.png";
+
 
 function TestFunction()
 {
-	context.drawImage(temp, 0, 0);
-	console.log(mapArray);
+    context.drawImage(tiles, 0, 0);
+}
+
+function DisplayMap()
+{
+    for (let i = 0; i < mapArray.length; i++)
+    {
+        for (let j = 0; j < mapArray.length; j++)
+        {
+            context.drawImage(tiles, 16 * i, 0, 16, 16, 16 * i, 16 * j, 16, 16);
+        }
+    }
 }
 
