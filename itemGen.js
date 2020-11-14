468a869944a6c95b1de8c89ffdfb295613ce4dbf
MethLabrador <62994899+MethLabrador@users.noreply.github.com>
2020-11-14 22:22:05 +0000

    adding makeWeapons
    u can laugh a bit more now

--- itemGen.js
+++ itemGen.js
@@ -22,7 +22,7 @@ class Item
 				break;
 
 			case "Weapon":
-				return "Weapon";
+				return this.makeWeapon();
 				break;
 
 			case "Armour":
@@ -38,19 +38,42 @@ class Item
 	makeTrinket()
 	{
 
-		//just doesnt work for some reason
+		//add more typesOfEffects, needs more funny ones
+		//takes random positions from typesOfTrinkets and typesOfEffects and concats them into one string
 		let finalItem = '';
-		let typesOfTrinkets = ["Amulet", "Ring", "Tooth", "Earring", "Spoon", "Mirror", "Cup", "Hairbrush", "Doorknob", "Book"];
+		let typesOfTrinkets = ["amulet", "ring", "tooth", "earring", "spoon", "mirror", "cup", "hairbrush", "doorknob", "book", "eye", "deck of cards", "bag of dice", "keychain"];
 		let typesOfEffects = ["that turns you invisible", "that makes you go mad", "that turns you into a wolf", "that allows you to hear to demons", "that turns you into a pickle"];
 
 		let randAdj = Math.floor(Math.random() * adjectives.length);
 		let randType = Math.floor(Math.random() * typesOfTrinkets.length);
 		let randEff = Math.floor(Math.random() * typesOfEffects.length);
 
-
-		finalItem = finalItem + " " + adjectives[randAdj] + " " + typesOfTrinkets[randType] + " " + typesOfEffects[randEff];
+		//this can be probably done using concat, stays like for now however
+		//i can not be bothered to make this nicer
+		finalItem = adjectives[randAdj] + " " + typesOfTrinkets[randType] + " " + typesOfEffects[randEff];
 		console.log(finalItem);
 		return finalItem;
+	}
+
+	makeWeapon()
+	{
+		
+		//attack bonus implement later
+		//figure out dictonaries and type it all into it 
+
+		let finalItem = '';
+
+		let typesOfWeapons = ["door", "sword", "axe", "bow", "shovel", "spear", "automatic rifle", "crossbow", "shortsword", "katana", "pike", "dead fish", "briefcase", "sack of doorknobs"]; 
+		let typesOfDamage = ["with fire damage", "with blunt damage", "with piercing damage", "with bludgeoning damage", "with cold damage", "with poison damage", "with acid damage", "with psychic damage", "with necrotic damage", "with radiant damage", "with force damage", "with thunder damage", "with lightning damage"];
+		
+		let randAdj = Math.floor(Math.random() * adjectives.length);
+		let randWep = Math.floor(Math.random() * typesOfWeapons.length);
+		let randDmg = Math.floor(Math.random() * typesOfDamage.length);
+
+		finalItem = '';
+		finalItem = adjectives[randAdj] + " " + typesOfWeapons[randWep] + " " + typesOfDamage[randDmg];
+		return finalItem;
+		
 	}
 
 }
@@ -59,5 +82,5 @@ function generateItem()
 {
 	let item = new Item();
 
-	document.getElementById("Item").innerHTML = item.item;
+	document.getElementById("Item").innerHTML = item.item + ".";
 }
