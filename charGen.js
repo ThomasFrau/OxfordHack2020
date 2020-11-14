06dccd003e384044c1a397ba7ecba27370ab22f6
MethLabrador <62994899+MethLabrador@users.noreply.github.com>
2020-11-14 12:38:39 +0000

    Some fixes
    removed .string() changed to .string

--- charGen.js
+++ charGen.js
@@ -4,8 +4,24 @@ function generateRandomName()
 	let secondNames = ["Poopy", "Smith", "Frau"];
 	
 	//Takes random index out of both firstNames and secondNames
-	let firstRandName = Math.floor(Math.random() * firstNames.length());
-	let secondRandName = Math.floor(Math.random() * secondNames.length());
+	//.length() is not a thing
+	let firstRandName = Math.floor(Math.random() * firstNames.length);
+	let secondRandName = Math.floor(Math.random() * secondNames.length);
 	
 	document.write(firstNames[firstRandName] + secondNames[secondRandName]);
-}	
+}
+
+function generateClass()
+{
+
+}
+
+function generateRace()
+{
+
+}
+
+function generateBackround()
+{
+
+}
