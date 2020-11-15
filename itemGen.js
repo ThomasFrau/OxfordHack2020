6011881373db9d2c3eb56bd38a7460ab9af8e7ee
MethLabrador <62994899+MethLabrador@users.noreply.github.com>
2020-11-15 15:18:32 +0000

    moar attribute

--- itemGen.js
+++ itemGen.js
@@ -1,4 +1,4 @@
-var adjectives = ["Seductive", "Dank", "Golden", "Silver", "Small", "Big", "Tiny", "Gigantic", "Heavy", "Light", "Diamond", "Wooden", "Expensive", "Cheap", "Dirty", "Used", "Brand-new", "Broken", "Exotic", "Powerful", "Burning", "Wireless", "Invisible", "Cringey", "Clean", "Creepy", "Useless", "German-made"];
+var adjectives = ["Boomerang-shaped", "Rhombus-shaped", "Circular", "Glass", "Seductive", "Dank", "Golden", "Silver", "Small", "Big", "Tiny", "Gigantic", "Heavy", "Light", "Diamond", "Wooden", "Expensive", "Cheap", "Dirty", "Used", "Brand-new", "Broken", "Exotic", "Powerful", "Burning", "Wireless", "Invisible", "Cringey", "Clean", "Creepy", "Useless", "German-made"];
 
 
 class Item {
@@ -99,8 +99,8 @@ class Item {
 		//add more typesOfEffects, needs more funny ones
 		//takes random positions from typesOfTrinkets and typesOfEffects and concats them into one string
 		let finalItem = '';
-		let typesOfTrinkets = ["dreamcatcher", "rubber duck", "cigarettes", "amulet", "ring", "tooth", "earring", "spoon", "mirror", "cup", "hairbrush", "doorknob", "book", "eye", "deck of cards", "bag of dice", "keychain"];
-		let typesOfEffects = ["turns you invisible", "makes you go mad", "turns you into a wolf", "allows you to hear to demons", "turns you into a pickle", "makes you really hungry all the time", "you can't take off", "makes you forget", "allows the demons to speak back"];
+		let typesOfTrinkets = ["vape pen", "dreamcatcher", "rubber duck", "cigarettes", "amulet", "ring", "tooth", "earring", "spoon", "mirror", "cup", "hairbrush", "doorknob", "book", "eye", "deck of cards", "bag of dice", "keychain"];
+		let typesOfEffects = ["aloows you to speak any language you wish", "makes you instantly cool", "never gets lost", "gives you the ability to see in the night", "that opens any door you wish", "never gets dirty", "dimly glows any colour you want it to", "gives you teleportation skills", "makes you laugh constantly", "turns you invisible", "makes you go mad", "turns you into a wolf", "allows you to hear to demons", "turns you into a pickle", "makes you really hungry all the time", "you can't take off", "makes you forget", "allows the demons to speak back", "makes you psychic", "makes you addicted to garlic bread"];
 
 		let randAdj = Math.floor(Math.random() * adjectives.length);
 		let randType = Math.floor(Math.random() * typesOfTrinkets.length);
