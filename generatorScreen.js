782499219f4edbfa669ee43ee4880ce9a045e790
ThomasFrau <32460955+ThomasFrau@users.noreply.github.com>
2020-11-15 01:53:46 +0000

    Paths done
    Added additional sprites and finished binary tree

--- generatorScreen.js
+++ generatorScreen.js
@@ -36,7 +36,7 @@ function CalculateTile(x,y)
     //3  4  5
     //6  7  8
 
-
+    //Love me some binary trees
     switch (v[4])
     {
         case 1:
@@ -47,8 +47,6 @@ function CalculateTile(x,y)
             return 0;      
         case 4:         //paths
             //v[0] != 4 && v[1] != 4 && v[2] != 4 && v[3] == 4 && v[5] == 4 && v[6] != 4 && v[7] != 4 && v[8] != 4
-<<<<<<< Updated upstream
-
             if (v[1] == 4 || v[1] == 5) {
                 if (v[7] == 4 || v[7] == 5) {
                     if (v[3] == 4 || v[3] == 5) {
@@ -117,18 +115,34 @@ function CalculateTile(x,y)
                             }
 
                         } else {
-                            if (v[0] != 4 && v[6] != 4) {
-                                return 70;
+                            if (v[0] != 4) {
+                                if (v[6] != 4) {
+                                    return 70;
+                                } else {
+                                    return 95;
+                                }                                
                             } else {
-                                return 61;
+                                if (v[6] != 4) {
+                                    return 96;
+                                } else {
+                                    return 61;
+                                }      
                             }                            
                         }
                     } else {
                         if (v[5] == 4 || v[5] == 5) {
-                            if (v[2] != 4 && v[8] != 4) {
-                                return 68;
+                            if (v[2] != 4) {
+                                if (v[8] != 4) {
+                                    return 68;
+                                } else {
+                                    return 93;
+                                }                                
                             } else {
-                                return 59;
+                                if (v[8] != 4) {
+                                    return 94;
+                                } else {
+                                    return 59;
+                                }                                
                             }                            
                         } else {
                             return 54;                       
@@ -137,10 +151,18 @@ function CalculateTile(x,y)
                 } else {
                     if (v[3] == 4 || v[3] == 5) {
                         if (v[5] == 4 || v[5] == 5) {
-                            if (v[0] != 4 && v[2] != 4) {
-                                return 72;
+                            if (v[0] != 4) {
+                                if (v[2] != 4) {
+                                    return 72;
+                                } else {
+                                    return 91;
+                                }                                
                             } else {
-                                return 63;
+                                if (v[2] != 4) {
+                                    return 92;
+                                } else {
+                                    return 63;
+                                }                                
                             }                            
                         } else {
                             if (v[0] != 4) {
@@ -165,10 +187,18 @@ function CalculateTile(x,y)
                 if (v[7] == 4 || v[7] == 5) {
                     if (v[3] == 4 || v[3] == 5) {
                         if (v[5] == 4 || v[5] == 5) {
-                            if (v[6] != 4 && v[8] != 4) {
-                                return 66;
+                            if (v[6] != 4) {
+                                if (v[8] != 4) {
+                                    return 66;
+                                } else {
+                                    return 89;
+                                }                                
                             } else {
-                                return 57;
+                                if (v[8] != 4) {
+                                    return 90;
+                                } else {
+                                    return 57;
+                                }                                
                             }                            
                         } else {
                             if (v[6] != 4) {
@@ -199,116 +229,13 @@ function CalculateTile(x,y)
                         if (v[5] == 4 || v[5] == 5) {
                             return 50;
                         } else {
-                            return 89;
+                            return 97;
                         }
                     }
                 }
             }     
-=======
-
-            if (v[1] == 4 || v[1] == 5) {
-                if (v[7] == 4 || v[7] == 5) {
-                    if (v[3] == 4 || v[3] == 5) {
-                        if (v[5] == 4 || v[5] == 5) {
-                            return 60;
-                        } else {
-                            return 61;
-                        }
-                    } else {
-                        if (v[5] == 4 || v[5] == 5) {
-                            return 59;
-                        } else {
-                            return 54;
-                        }
-                    }
-                } else {
-                    if (v[3] == 4 || v[3] == 5) {
-                        if (v[5] == 4 || v[5] == 5) {
-                            return 63;
-                        } else {
-                            return 64;
-                        }
-                    } else {
-                        if (v[5] == 4 || v[5] == 5) {
-                            return 62;
-                        } else {
-                            return 55;
-                        }
-                    }
-                }
-            } else {
-                if (v[7] == 4 || v[7] == 5) {
-                    if (v[3] == 4 || v[3] == 5) {
-                        if (v[5] == 4 || v[5] == 5) {
-                            return 57;
-                        } else {
-                            return 58;
-                        }
-                    } else {
-                        if (v[5] == 4 || v[5] == 5) {
-                            return 56;
-                        } else {
-                            return 53;
-                        }
-                    }
-                } else {
-                    if (v[3] == 4 || v[3] == 5) {
-                        if (v[5] == 4 || v[5] == 5) {
-                            return 51;
-                        } else {
-                            return 52;
-                        }
-                    } else {
-                        if (v[5] == 4 || v[5] == 5) {
-                            return 50;
-                        } else {
-                            return 83;
-                        }
-                    }
-                }
-            }
-
-
-
-
-            /*if (v[1] != 4 && v[3] != 4 && v[5] == 4 && v[7] != 4) {
-                return 50;
-            } else if (v[1] != 4 && v[3] == 4 && v[5] == 4 && v[7] != 4) {
-                return 51;
-            } else if (v[1] != 4 && v[3] == 4 && v[5] != 4 && v[7] != 4) {
-                return 52;
-            } else if (v[1] != 4 && v[3] != 4 && v[5] != 4 && v[7] == 4) {
-                return 53;
-            } else if (v[1] == 4 && v[3] != 4 && v[5] != 4 && v[7] == 4) {
-                return 54;
-            } else if (v[1] == 4 && v[3] != 4 && v[5] != 4 && v[7] != 4) {
-                return 55;
-            } else if (v[1] != 4 && v[3] != 4 && v[5] == 4 && v[7] == 4) {
-                return 56;
-            } else if (v[1] != 4 && v[3] == 4 && v[5] == 4 && v[7] == 4) {
-                return 57;
-            } else if (v[1] != 4 && v[3] == 4 && v[5] != 4 && v[7] == 4) {
-                return 58;
-            } else if (v[1] == 4 && v[3] != 4 && v[5] == 4 && v[7] == 4) {
-                return 59;
-            } else if (v[1] == 4 && v[3] == 4 && v[5] == 4 && v[7] == 4) {
-                return 60;
-            } else if (v[1] == 4 && v[3] == 4 && v[5] != 4 && v[7] == 4) {
-                return 61;
-            } else if (v[1] == 4 && v[3] != 4 && v[5] == 4 && v[7] != 4) {
-                return 62;
-            } else if (v[1] == 4 && v[3] == 4 && v[5] == 4 && v[7] != 4) {
-                return 63;
-            } else if (v[1] == 4 && v[3] == 4 && v[5] != 4 && v[7] != 4) {
-                return 64;
-            }
-
-            else {
-                return 83;
-            }*/
->>>>>>> Stashed changes
         case 5:
-            return 100;
+            return 108;
         case 6:
             return 3;
         case 7:
